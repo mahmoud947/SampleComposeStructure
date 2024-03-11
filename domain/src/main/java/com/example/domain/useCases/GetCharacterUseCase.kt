c8508bfdee27296bd8046b50fe31f8ec1dc5f5d2
Mahmoud kamal <mahmoudkamal4673@gmail.com>
2024-03-11 04:58:22 +0200

    implement GetCharacterUseCase (domain layer)

---
+++ domain/src/main/java/com/example/domain/useCases/GetCharacterUseCase.kt
@@ -0,0 +1,25 @@
+package com.example.domain.useCases
+
+import com.example.core.base.BaseUseCase
+import com.example.domain.errors.ExceptionHandler
+import com.example.domain.models.Character
+import com.example.domain.repositories.CharactersRepository
+import com.example.domain.utils.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+
+class GetCharacterUseCase constructor(
+    private val charactersRepository: CharactersRepository
+) : BaseUseCase<Nothing?, Flow<Resource<List<Character>?>>> {
+    override fun invoke(input: Nothing?): Flow<Resource<List<Character>?>> {
+        return flow {
+            emit(Resource.Loading)
+            val result = charactersRepository.getCharacters()
+            emit(Resource.Success(result.results))
+        }.catch {
+            emit(ExceptionHandler.resolveError(it))
+        }
+    }
+}
+
